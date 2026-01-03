a26c29537ec0315b36cd11c1b0bd55434335f7e5
ArthurDombroski <tutudombroski@gmail.com>
2026-01-03 11:19:26 -0300

    initial commit

---
+++ backend/src/main/java/com/project/math/LinearMathProApplication.java
@@ -0,0 +1,13 @@
+package com.project.math;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinearMathProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinearMathProApplication.class, args);
+	}
+
+}
