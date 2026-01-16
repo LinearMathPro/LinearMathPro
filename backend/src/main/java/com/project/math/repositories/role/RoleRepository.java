be4f99dfe314f7646ae3aeb2bb34428d1bbeab27
João Fabiano <jpfabiano08@gmail.com>
2026-01-15 21:07:19 -0300

    Add : metodos nos repositórios de Usuario e Role

--- backend/src/main/java/com/project/math/repositories/role/RoleRepository.java
+++ backend/src/main/java/com/project/math/repositories/role/RoleRepository.java
@@ -1,9 +1,13 @@
 package com.project.math.repositories.role;
 
 import com.project.math.models.role.RoleModel;
+import com.project.math.models.role.TipoRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<RoleModel, Long> {
+    Optional<RoleModel> findByNomeRole (TipoRole nome);
 }
--- backend/src/main/java/com/project/math/repositories/usuario/UsuarioRoleRepository.java
+++ backend/src/main/java/com/project/math/repositories/usuario/UsuarioRoleRepository.java
@@ -1,8 +1,11 @@
 package com.project.math.repositories.usuario;
 
+import com.project.math.models.role.RoleModel;
 import com.project.math.models.usuario.UsuarioRoleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRoleRepository extends JpaRepository<UsuarioRoleModel, Long> {
