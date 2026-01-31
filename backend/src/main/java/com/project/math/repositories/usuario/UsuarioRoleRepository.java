aa837fca96e878de66b9257573f1a28edaff7b67
João Fabiano <jpfabiano08@gmail.com>
2026-01-31 12:39:07 -0300

    Add : metodos no UsuarioRepository

--- backend/src/main/java/com/project/math/repositories/usuario/UsuarioRoleRepository.java
+++ backend/src/main/java/com/project/math/repositories/usuario/UsuarioRoleRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface UsuarioRoleRepository extends JpaRepository<UsuarioRoleModel, Long> {
+    Optional<UsuarioRoleModel> findByUsuario_IdUsuario(Long idUsuario);
 }
